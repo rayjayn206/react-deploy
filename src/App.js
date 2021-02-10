9cf0753f926575dcf5922462cc891ec7d3673a29
rayjayn206 <rayjayn206@gmail.com>
2021-02-09 16:25:46 -0800

    deployin app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>react app</h1>
+      <header className="App-header">
+   
+      </header>
+    </div>
+  );
+}
+
+export default App;
